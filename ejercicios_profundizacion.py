55fb46d74160e2ff3eac178098913eab2f79ec42
eotorresmolina <64936281+eotorresmolina@users.noreply.github.com>
2020-09-22 19:40:58 -0300

    Update
    Cambio en el Nombre de Autor.

--- ejercicios_profundizacion.py
+++ ejercicios_profundizacion.py
@@ -11,8 +11,8 @@ Programa creado para que practiquen los conocimietos
 adquiridos durante la semana
 '''
 
-__author__ = "Inove Coding School"
-__email__ = "alumnos@inove.com.ar"
+__author__ = "Emmanuel Oscar Torres Molina"
+__email__ = "emmaotm@gmail.com"
 __version__ = "1.1"
 
 import numpy as np
